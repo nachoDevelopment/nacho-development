9032509569cf845a878c7a150484cab66e5efe49
Daniel Schmid <danielschmid@Daniels-MacBook-Air.local>
2021-02-14 13:43:17 +0000

    initial commit

---
+++ components/footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <a
+        className="footer__link"
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Designed & Built By _nachoDevelopment
+      </a>
+    </footer>
+  )
+}
